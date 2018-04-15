85ee04413d6f1b61a162d7832dd972b0e56acb20
moerphin <marcos.hike@gmail.com>
2018-04-15 15:33:51 +0200

    Transfer speed added

--- vzdump2influx.sh
+++ vzdump2influx.sh
@@ -11,13 +11,16 @@ if [ "$1" == "backup-start" ]; then
     echo `date +%s` > /tmp/backup-info
     echo $HOSTNAME >> /tmp/backup-info
 fi
-if [ "$1" == "backup-end" ]; then
-    DURATION=$((`date +%s`-`sed '1q;d' /tmp/backup-info`))
-    /usr/bin/curl -s -i -XPOST -u $DBUSER:$DBPASS "http://$DBHOST:$DBPORT/write?db=$DBNAME" --data-binary  "backup_px,host=$HOSTNAME,location=$LOCATIONCODE success=1,duration=$DURATION,size=`stat -c%s $TARFILE`" > /dev/null
-    rm /tmp/backup-info
-fi
-if [ "$1" == "backup-abort" ]; then
-    DURATION=$((`date +%s`-`sed '1q;d' /tmp/backup-info`))
-    /usr/bin/curl -s -i -XPOST -u $DBUSER:$DBPASS "http://$DBHOST:$DBPORT/write?db=$DBNAME" --data-binary  "backup_px,host=$HOSTNAME,location=$LOCATIONCODE success=0,duration=$DURATION,size=0" > /dev/null
-    rm /tmp/backup-info
+
+if [ "$1" == "log-end" ]; then
+    if [ `cat ${LOGFILE} | grep ERROR | wc -l` -gt 0 ]; then
+        DURATION=$((`date +%s`-`sed '1q;d' /tmp/backup-info`))
+        /usr/bin/curl -s -i -XPOST -u $DBUSER:$DBPASS "http://$DBHOST:$DBPORT/write?db=$DBNAME" --data-binary  "backup_px,host=$HOSTNAME,location=$LOCATIONCODE success=0,duration=$DURATION,speed=0,size=0" > /dev/null
+        rm /tmp/backup-info
+    else
+        SPEED=$((`cat ${LOGFILE} | grep -o -P "(?<=seconds \().*(?= MB/s)"`))
+        DURATION=$((`cat ${LOGFILE} | grep -o -P "(?<=MB in ).*(?= seconds)"`))
+        /usr/bin/curl -s -i -XPOST -u $DBUSER:$DBPASS "http://$DBHOST:$DBPORT/write?db=$DBNAME" --data-binary  "backup_px,host=$HOSTNAME,location=$LOCATIONCODE success=1,duration=$DURATION,speed=$SPEED,size=`stat -c%s $TARFILE`" > /dev/null
+        rm /tmp/backup-info
+    fi
 fi
