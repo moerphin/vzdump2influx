7b8a50e4ef3d6f84b60e19ead88f88a21370fb8f
moerphin <marcos.hike@gmail.com>
2019-05-03 11:36:34 +0200

    LXC speed and duration fix

--- vzdump2influx.sh
+++ vzdump2influx.sh
@@ -15,15 +15,15 @@ fi
 if [ "$1" == "log-end" ]; then
     if [ `cat ${LOGFILE} | grep ERROR | wc -l` -gt 0 ]; then
         DURATION=$((`date +%s`-`sed '1q;d' /tmp/backup-info`))
-        /usr/bin/curl -s -i -XPOST -u $DBUSER:$DBPASS "http://$DBHOST:$DBPORT/write?db=$DBNAME" --data-binary  "backup_px,host=$HOSTNAME,location=$LOCATIONCODE success=0,duration=$DURATION,speed=0,size=0" > /dev/null
+        /usr/bin/curl -s -i -XPOST -u $DBUSER:$DBPASS "http://$DBHOST:$DBPORT/write?db=$DBNAME" --data-binary  "proxmox,host=$HOSTNAME,location=$LOCATIONCODE success=0,duration=$DURATION,speed=0,size=0" > /tmp/tst
         rm /tmp/backup-info
     else
         SPEED=$((`cat ${LOGFILE} | grep -o -P "(?<=seconds \().*(?= MB/s)"`))
         if [ ! "$SPEED" -gt 0 ]; then
-            SPEED=$((`cat ${LOGFILE} | grep -o -P "(?<=.iB, ).*(?=.iB\/s)"`))
+            SPEED=$(cat ${LOGFILE} | grep -o -P "(?<=.iB, ).*(?=.iB\/s)")
         fi
-        DURATION=$((`cat ${LOGFILE} | grep -o -P "(?<=MB in ).*(?= seconds)"`))
-        /usr/bin/curl -s -i -XPOST -u $DBUSER:$DBPASS "http://$DBHOST:$DBPORT/write?db=$DBNAME" --data-binary  "backup_px,host=$HOSTNAME,location=$LOCATIONCODE success=1,duration=$DURATION,speed=$SPEED,size=`stat -c%s $TARFILE`" > /dev/null
-        rm /tmp/backup-info
+        DURATION=$((`cat ${LOGFILE} |grep -o -P "(?<=\()[0-9][0-9]:[0-9][0-9]:[0-9][0-9](?=\))"|awk -F':' '{print($1*3600)+($2*60)+$3}'`))
+        /usr/bin/curl -s -i -XPOST -u $DBUSER:$DBPASS "http://$DBHOST:$DBPORT/write?db=$DBNAME" --data-binary  "proxmox,host=$HOSTNAME,location=$LOCATIONCODE success=1,duration=$DURATION,speed=$SPEED,size=`stat -c%s $TARFILE`" > /tmp/tst
+        echo $TRAFILE >> /tmp/tst
     fi
 fi
