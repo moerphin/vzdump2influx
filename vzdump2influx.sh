2a064ee1a7bd7cc9ad43ca02d12529d591da7767
moerphin <marcos.hike@gmail.com>
2020-07-22 14:39:48 +0200

    typo

--- vzdump2influx.sh
+++ vzdump2influx.sh
@@ -30,6 +30,6 @@ if [ "$1" == "log-end" ]; then
         fi
         DURATION=$((`cat ${LOGFILE} |grep -o -P "(?<=\()[0-9][0-9]:[0-9][0-9]:[0-9][0-9](?=\))"|awk -F':' '{print($1*3600)+($2*60)+$3}'`))
         /usr/bin/curl -s -i -XPOST -u $DBUSER:$DBPASS "$DBPROTO://$DBHOST:$DBPORT/write?db=$DBNAME" --data-binary  "proxmox,host=$HOSTNAME,location=$LOCATIONCODE success=1,duration=$DURATION,speed=$SPEED,size=`stat -c%s $TARFILE`" > /tmp/tst
-        echo $TRAFILE >> /tmp/tst
+        echo $TARFILE >> /tmp/tst
     fi
 fi
