cc85ceedcfe5fe031af95b32ef69a46d5e8f10ba
moerphin <marcos.hike@gmail.com>
2020-07-22 13:14:43 +0200

    float patch

--- vzdump2influx.sh
+++ vzdump2influx.sh
@@ -8,6 +8,7 @@ DBPORT=<DBPORT>
 DBNAME=<DBNAME>
 LOCATIONCODE=<LOCATION_CODE>
 DEBUG=false #Debug mode, copy all logs to /tmp/timestamp
+SPEED=""
 
 if [ "$1" == "backup-start" ]; then
     echo `date +%s` > /tmp/backup-info
@@ -23,9 +24,9 @@ if [ "$1" == "log-end" ]; then
         /usr/bin/curl -s -i -XPOST -u $DBUSER:$DBPASS "$DBPROTO://$DBHOST:$DBPORT/write?db=$DBNAME" --data-binary  "proxmox,host=$HOSTNAME,location=$LOCATIONCODE success=0,duration=$DURATION,speed=0,size=0" > /tmp/tst
         rm /tmp/backup-info
     else
-        SPEED=$((`cat ${LOGFILE} | grep -o -P "(?<=seconds \().*(?= MB\/s| MiB\/s)"`))
-        if [ ! "$SPEED" -gt 0 ]; then
-            SPEED=$(cat ${LOGFILE} | grep -o -P "(?<=.iB, ).*(?=.iB\/s)")
+        SPEED=`cat ${LOGFILE} | grep -o -P "(?<=seconds \().*(?= MB\/s| MiB\/s)"`
+        if [ -z $SPEED ]; then
+            SPEED=`cat ${LOGFILE} | grep -o -P "(?<=.iB, ).*(?=.iB\/s)"`
         fi
         DURATION=$((`cat ${LOGFILE} |grep -o -P "(?<=\()[0-9][0-9]:[0-9][0-9]:[0-9][0-9](?=\))"|awk -F':' '{print($1*3600)+($2*60)+$3}'`))
         /usr/bin/curl -s -i -XPOST -u $DBUSER:$DBPASS "$DBPROTO://$DBHOST:$DBPORT/write?db=$DBNAME" --data-binary  "proxmox,host=$HOSTNAME,location=$LOCATIONCODE success=1,duration=$DURATION,speed=$SPEED,size=`stat -c%s $TARFILE`" > /tmp/tst
