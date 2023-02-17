a1749850c54b957f29e784ff43c63d6a6661d01c
moerphin <marcos.hike@gmail.com>
2023-02-17 09:52:04 +0100

    PBS support

--- vzdump2influx.sh
+++ vzdump2influx.sh
@@ -24,11 +24,42 @@ if [ "$1" == "log-end" ]; then
         /usr/bin/curl --request POST "$PROTOCOL://$DBHOSTNAME:$PORT/api/v2/write?org=$ORGANIZATION&bucket=$BUCKETNAME&precision=ns" --data-binary  "proxmox,host=$HOSTNAME,location=$LOCATIONCODE success=0,duration=$DURATION,speed=0,size=0" --header "Authorization: Token $TOKEN" --header "Content-Type: text/plain; charset=utf-8" --header "Accept: application/json"
         rm /tmp/backup-info
     else
-        SPEED=`cat ${LOGFILE} | grep -o -P "(?<=seconds \().*(?= MB\/s| MiB\/s)"`
-        if [ -z $SPEED ]; then
-            SPEED=`cat ${LOGFILE} | grep -o -P "(?<=.iB, ).*(?=.iB\/s)"`
+        if [ -f "${TARGET}" ]; then
+            SPEED=`cat ${LOGFILE} | grep -o -P "(?<=seconds \().*(?= MB\/s| MiB\/s)"`
+            if [ -z $SPEED ]; then
+                SPEED=`cat ${LOGFILE} | grep -o -P "(?<=.iB, ).*(?=.iB\/s)"`
+            fi
+            DURATION=$((`cat ${LOGFILE} |grep -o -P "(?<=\()[0-9][0-9]:[0-9][0-9]:[0-9][0-9](?=\))"|awk -F':' '{print($1*3600)+($2*60)+$3}'`))
+            /usr/bin/curl --request POST "$PROTOCOL://$DBHOSTNAME:$PORT/api/v2/write?org=$ORGANIZATION&bucket=$BUCKETNAME&precision=ns" --data-binary  "proxmox,host=$HOSTNAME,location=$LOCATIONCODE success=1,duration=$DURATION,speed=$SPEED,size=`stat -c%s $TARGET`" --header "Authorization: Token $TOKEN" --header "Content-Type: text/plain; charset=utf-8" --header "Accept: application/json"
+        else
+            SPEEDR=$((`cat ${LOGFILE} |grep -o -P "(?<=\().*(?=....\/s\))"`))
+            SPEEDS=$((`cat ${LOGFILE} |grep -o -P "(?<=[0-9] ).(?=iB\/s\))"`))
+            case $SPEEDS in
+                K)
+                    SPEED=`echo "$SPEEDR 1024" | awk '{printf "%f", $1 / $2}'`
+                ;;
+                M)
+                    SPEED=$SPEEDR
+                ;;
+                G)
+                    SPEED=`echo "$SPEEDR 1024" | awk '{printf "%f", $1 * $2}'`
+                ;;
+            esac
+            DURATION=$((`cat ${LOGFILE} |grep -o -P "(?<=\()[0-9][0-9]:[0-9][0-9]:[0-9][0-9](?=\))"|awk -F':' '{print($1*3600)+($2*60)+$3}'`))
+            SIZER=$((`cat ${LOGFILE} |grep -o -P "(?<=transferred ).*(?= [K|M|G]iB in )"`))
+            SIZES=$((`cat ${LOGFILE} |grep -o -P "(?<=[0-9 ]).(?=iB in [0-9])"`))
+            case $SIZES in
+                K)
+                    SIZE=`echo "$SIZER 1024" | awk '{printf "%f", $1 / $2}'`
+                ;;
+                M)
+                    SIZE=$SIZER
+                ;;
+                G)
+                    SIZE=`echo "$SIZER 1024" | awk '{printf "%f", $1 * $2}'`
+                ;;
+            esac
+            /usr/bin/curl --request POST "$PROTOCOL://$DBHOSTNAME:$PORT/api/v2/write?org=$ORGANIZATION&bucket=$BUCKETNAME&precision=ns" --data-binary  "proxmox,host=$HOSTNAME,location=$LOCATIONCODE success=1,duration=$DURATION,speed=$SPEED,size=$SIZE" --header "Authorization: Token $TOKEN" --header "Content-Type: text/plain; charset=utf-8" --header "Accept: application/json"
         fi
-        DURATION=$((`cat ${LOGFILE} |grep -o -P "(?<=\()[0-9][0-9]:[0-9][0-9]:[0-9][0-9](?=\))"|awk -F':' '{print($1*3600)+($2*60)+$3}'`))
-        /usr/bin/curl --request POST "$PROTOCOL://$DBHOSTNAME:$PORT/api/v2/write?org=$ORGANIZATION&bucket=$BUCKETNAME&precision=ns" --data-binary  "proxmox,host=$HOSTNAME,location=$LOCATIONCODE success=1,duration=$DURATION,speed=$SPEED,size=`stat -c%s $TARGET`" --header "Authorization: Token $TOKEN" --header "Content-Type: text/plain; charset=utf-8" --header "Accept: application/json"
     fi
 fi
